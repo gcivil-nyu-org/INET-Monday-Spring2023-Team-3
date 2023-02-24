7961e2326b05f294a3e7431f4337e1683785cff7
dhauss <davidkhauss@gmail.com>
2023-02-24 10:11:59 -0500

    added local host to allowed hosts in settings.py

--- nghbrly/nghbrly/settings.py
+++ nghbrly/nghbrly/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nghbrly.eba-qcpx8k2g.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['nghbrly.eba-qcpx8k2g.us-east-1.elasticbeanstalk.com',
+                 '127.0.0.1']
 
 
 # Application definition
