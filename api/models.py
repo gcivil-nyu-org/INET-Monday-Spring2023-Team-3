b064b17da7a48fd3336acb9e76a763e016ea81dc
Anil Poonai <50726395+DevonARP@users.noreply.github.com>
2023-03-05 13:57:08 -0500

    Pushing repo up a level

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    username = models.TextField()
+    email = models.TextField()
+    password = models.TextField()
+
+    def _str_(self):
+        return self.username
