0e2acc856bd5491cd1010a16bc876fdd7119035f
dhauss <davidkhauss@gmail.com>
2023-03-13 10:50:07 -0400

    added on_delete option to foreign keys in through tables

--- crawls/models.py
+++ crawls/models.py
@@ -4,47 +4,60 @@ from django.db import models
 
 
 class User(models.Model):
-	username = models.TextField()
-	email = models.TextField()
-	password = models.TextField()
+    username = models.TextField()
+    email = models.TextField()
+    password = models.TextField()
 
-	def _str_(self):
-		return self.username
+    def _str_(self):
+        return self.username
 
 
 class Tag(models.Model):
-	name = models.CharField(max_length=200)
-	tag = models.ManyToManyField('Tag', through='TagPoint')
+    title = models.CharField(max_length=200)
+    # tag = models.ManyToManyField('Tag', through='TagPoint')
+
+    def __str__(self):
+        return self.title
 
 
 class Crawl(models.Model):
-	name = models.CharField(max_length=200)
-	author = models.CharField(max_length=200)
-	created_at = models.DateTimeField(auto_now_add=True)
-	description = models.TextField(max_length=500, null=True)
-	tags = models.ManyToManyField(Tag, null=True)
-	points = models.ManyToManyField('Point', through='CrawlPoint', null=True, blank=True)
-	# Number_of_point = models.IntegerField()
-	# Total_length_in_miles = models.FloatField()
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(max_length=500, null=True)
+    tags = models.ManyToManyField(Tag, null=True)
+    points = models.ManyToManyField(
+        'Point', through='CrawlPoint', null=True, blank=True
+        )
+    # Number_of_point = models.IntegerField()
+    # Total_length_in_miles = models.FloatField()
+
+    def _str_(self):
+        return self.name
 
 
 class Point(models.Model):
-	name = models.CharField(max_length=200)
-	google_place_id = models.TextField(null=True)  # until we figure out GMaps API
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now_add=True)
-	longitude = models.FloatField(null=True)
-	latitude = models.FloatField(null=True)
-	description = models.TextField(max_length=500, null=True)
-	address = models.TextField(max_length=200, null=True)  # until we figure out GMaps API
-	crawls = models.ManyToManyField('Crawl', through='CrawlPoint', null=True, blank=True)
+    name = models.CharField(max_length=200)
+    google_place_id = models.TextField(null=True)  # until we figure out GMaps API
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    longitude = models.FloatField(null=True)
+    latitude = models.FloatField(null=True)
+    description = models.TextField(max_length=500, null=True)
+    address = models.TextField(max_length=200, null=True)  # until we figure out GM API
+    crawls = models.ManyToManyField(
+        'Crawl', through='CrawlPoint', null=True, blank=True
+        )
+
+    def _str_(self):
+        return self.name
 
 
 class CrawlPoint(models.Model):
-	crawl = models.ForeignKey(Crawl)
-	point = models.ForeignKey(Point)
+    crawl = models.ForeignKey(Crawl, on_delete=models.CASCADE)
+    point = models.ForeignKey(Point, on_delete=models.CASCADE)
 
 
 class TagPoint(models.Model):
-	tag = models.ForeignKey(Tag)
-	point = models.ForeignKey(Point)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    point = models.ForeignKey(Point, on_delete=models.CASCADE)
