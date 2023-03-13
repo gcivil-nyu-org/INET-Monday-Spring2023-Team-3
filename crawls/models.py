c9fce3ea7af84a2b90f15924fbb5c2348c4e0709
dhauss <davidkhauss@gmail.com>
2023-03-13 10:06:27 -0400

    flake8 fixes on crawl models

--- crawls/models.py
+++ crawls/models.py
@@ -4,41 +4,46 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.TextField()
-    email = models.TextField()
-    password = models.TextField()
+	username = models.TextField()
+	email = models.TextField()
+	password = models.TextField()
 
-    def _str_(self):
-        return self.username
+	def _str_(self):
+		return self.username
+
 
 class Tag(models.Model):
-	Name = models.CharField(max_length=200)
-	Tag = models.ManyToManyField('Tag', through='TagPoint')
+	name = models.CharField(max_length=200)
+	tag = models.ManyToManyField('Tag', through='TagPoint')
+
 
 class Crawl(models.Model):
-	Name = models.CharField(max_length=200)
-	Author = models.CharField(max_length=200)
-	Created_at = models.DateTimeField(auto_now_add=True)
-	Description = models.TextField(max_length=500, null=True)
-	Tags = models.ManyToManyField(Tag, null=True)
-	Points = models.ManyToManyField('Point', through='CrawlPoint', null=True, blank=True)
-	##Number_of_point = models.IntegerField()
-	##Total_length_in_miles = models.FloatField()
+	name = models.CharField(max_length=200)
+	author = models.CharField(max_length=200)
+	created_at = models.DateTimeField(auto_now_add=True)
+	description = models.TextField(max_length=500, null=True)
+	tags = models.ManyToManyField(Tag, null=True)
+	points = models.ManyToManyField('Point', through='CrawlPoint', null=True, blank=True)
+	# Number_of_point = models.IntegerField()
+	# Total_length_in_miles = models.FloatField()
+
 
 class Point(models.Model):
-	Name = models.CharField(max_length=200)
-	Google_Place_ID = models.TextField(null=True)  #until we figure out GMaps API
-	Created_at = models.DateTimeField(auto_now_add=True)
-	Updated_at = models.DateTimeField(auto_now_add=True)
-	Longitude = models.FloatField(null=True)
-	Latitude = models.FloatField(null=True)
-	Description = models.TextField(max_length=500, null=True)
-	Address = models.TextField(max_length=200, null=True) #until we figure out GMaps API
-	Crawls =  models.ManyToManyField('Crawl', through='CrawlPoint', null=True, blank=True)
+	name = models.CharField(max_length=200)
+	google_place_id = models.TextField(null=True)  # until we figure out GMaps API
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+	longitude = models.FloatField(null=True)
+	latitude = models.FloatField(null=True)
+	description = models.TextField(max_length=500, null=True)
+	address = models.TextField(max_length=200, null=True)  # until we figure out GMaps API
+	crawls = models.ManyToManyField('Crawl', through='CrawlPoint', null=True, blank=True)
+
 
 class CrawlPoint(models.Model):
 	crawl = models.ForeignKey(Crawl)
 	point = models.ForeignKey(Point)
+
 
 class TagPoint(models.Model):
 	tag = models.ForeignKey(Tag)
