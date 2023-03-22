bc253df59e3f4286241ba59bd928e273e48a5901
Anil Poonai <50726395+DevonARP@users.noreply.github.com>
2023-03-22 09:51:47 -0400

    Update test_models.py

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -5,9 +5,9 @@ from api.models import User
 class TestModels(TestCase):
     def setUp(self):
         return User.objects.create(
-            username="tester1", email="tester@gmail.com", password="password123"
+            username="testanil", email="testanil@gmail.com", password="password123"
         )
 
     def test_user(self):
         u = self.setUp()
-        self.assertEquals("tester1", u.username)
+        self.assertEquals("testanil", u.username)
