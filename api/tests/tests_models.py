f2395c1516596344d4dab0f552ead71b54a3a321
dhauss <davidkhauss@gmail.com>
2023-03-22 10:13:02 -0400

    formatting fixes

--- api/tests/tests_models.py
+++ api/tests/tests_models.py
@@ -4,9 +4,7 @@ from api.models import User
 
 class TestModels(TestCase):
     def setUp(self):
-        return User.objects.create(
-            username="testanil"
-        )
+        return User.objects.create(username="testanil")
 
     def test_user(self):
         u = self.setUp()
