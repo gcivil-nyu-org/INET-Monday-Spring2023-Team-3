680b4f58bc6acaf0937e4d7b2e90d29fe24ce06f
dhauss <davidkhauss@gmail.com>
2023-03-24 11:08:49 -0400

    format fail, fixing first two api view tests

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -2,26 +2,19 @@ from rest_framework.test import APITestCase  # , APIRequestFactory
 from rest_framework import status
 from django.urls import reverse
 from api.models import User
+
 # from api.views import user_register
 
 
 class RegisterTest(APITestCase):
     def test_user_register(self):
-        data = {
-            "username": "test_u",
-            "email": "test@gmail.com",
-            "password": "test_pw"
-        }
+        data = {"username": "test_u", "email": "test@gmail.com", "password": "test_pw"}
 
         response = self.client.post(reverse("register"), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_user_register_fail(self):
-        data = {
-            "username": "test_u",
-            "email": "test@gmail.com",
-            "password": "test_pw"
-        }
+        data = {"username": "test_u", "email": "test@gmail.com", "password": "test_pw"}
 
         User.objects.create(**data)
 
