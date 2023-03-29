6537ea4cc8516de0aae4d9dbd508118e9919002a
ChungEun Jennifer Chae <cchae216@gmail.com>
2023-03-29 16:06:11 -0400

    fixing issues with building..

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ class UserTest(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     location = models.CharField(max_length=50, null=True, blank=True)
     short_bio = models.CharField(max_length=200, null=True)
-    
+
     def __str__(self):
         return self.username
 
@@ -24,7 +24,7 @@ class User(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     location = models.CharField(max_length=50, null=True, blank=True)
     short_bio = models.CharField(max_length=200, null=True, blank=True)
-    
+
     def __str__(self):
         return self.username
 
