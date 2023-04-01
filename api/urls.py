6bf27f9c05911ce33157a4bca90a2596cfe7bb25
ChungEun Jennifer Chae <cchae216@gmail.com>
2023-03-31 21:55:01 -0400

    resolve build issue with verify

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("register/", views.user_register, name="register"),
     path("login/", views.user_login, name="login"),
     path("profile/<str:other_username>/", views.profile, name="profile"),
-    path("verify/", views.email_verify),
+    path("verify/", views.email_verify, name="verify"),
     path("send-otp/", views.send_otp, name="send_otp"),
     path("recover/", views.send_recovery, name="recover"),
     path("verify-recovery/", views.verify_recovery),
