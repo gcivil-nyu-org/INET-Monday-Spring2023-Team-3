f8c5234f9effadc90e960cf032b4e78790c53470
dhauss <davidkhauss@gmail.com>
2023-04-02 14:16:48 -0400

    added crawl create tests, crawl delete fail

--- crawls/tests/test_views.py
+++ crawls/tests/test_views.py
@@ -20,7 +20,7 @@ class TestCrawls(APITestCase):
 
         self.client.credentials(HTTP_AUTHORIZATION=jwt)
 
-    def test_crawl_get(self):
+    def test_crawl_get_all(self):
         self.authenticate()
 
         crawl_data = {"google_place_id": "12335"}
@@ -43,7 +43,7 @@ class TestCrawls(APITestCase):
         self.assertEqual(response.data[0]["title"], "sample_crawl")
         self.assertEqual(response.data[1]["title"], "sample_crawl_2")
 
-    def test_crawl_delete(self):
+    def test_crawl_delete_success(self):
         self.authenticate()
 
         crawl_data = {"google_place_id": "12335"}
@@ -56,3 +56,38 @@ class TestCrawls(APITestCase):
 
         response = self.client.post(reverse("crawl_delete"), {"title": "sample_crawl"})
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+    def test_crawl_delete_fail(self):
+        self.authenticate()
+
+        response = self.client.post(reverse("crawl_delete"), {"title": "sample_crawl"})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_crawl_create_success(self):
+        self.authenticate()
+
+        crawl_data = {"google_place_id": "12335"}
+        data = {
+            "title": "sample_crawl",
+            "author": "test_u",
+            "data": json.dumps(crawl_data),
+        }
+
+        response = self.client.post(reverse("crawl_create"), data)
+        crawl = Crawl.objects.all()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(crawl[0].title, "sample_crawl")
+
+    def test_crawl_create_fail(self):
+        self.authenticate()
+
+        crawl_data = {"google_place_id": "12335"}
+        data = {
+            "title": "sample_crawl",
+            "author": "test_u",
+            "data": json.dumps(crawl_data),
+        }
+
+        Crawl.objects.create(**data)
+        response = self.client.post(reverse("crawl_create"), data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
