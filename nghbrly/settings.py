75ea7d072d151334e72c51f51c466276dc40fc36
ismaeltocruz <ismaeltocruz@gmail.com>
2023-04-03 11:17:05 -0400

    add rest framework to apps

--- nghbrly/settings.py
+++ nghbrly/settings.py
@@ -49,9 +49,14 @@ INSTALLED_APPS = [
     "corsheaders",
     "api",
     "crawls.apps.CrawlsConfig",
+    # "api.apps.ApiConfig",
+    "rest_framework",
+
+
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -59,7 +64,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
+
 ]
 
 ROOT_URLCONF = "nghbrly.urls"
