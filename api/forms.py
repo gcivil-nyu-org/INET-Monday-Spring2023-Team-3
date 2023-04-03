7f2a2e0a88fbb41c9224c0aa96e30870de8ee1f5
ismaeltocruz <ismaeltocruz@gmail.com>
2023-04-03 11:18:38 -0400

    forms for testing image upload

---
+++ api/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from django import forms
+
+from .models import User
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'profile_pic', 'short_bio',)
