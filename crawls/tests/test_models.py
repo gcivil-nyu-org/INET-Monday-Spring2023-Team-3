dfb84b3df4d2dbb7b5bc3c547029f13a7e9079ed
dhauss <davidkhauss@gmail.com>
2023-04-10 11:43:27 -0400

    update tests for new crawl model

--- crawls/tests/test_models.py
+++ crawls/tests/test_models.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
 from crawls.models import Crawl, Point, Tag, CrawlPoint, CrawlTag
+from api.models import User
 
 
 class TestModels(TestCase):
-    def setUp(self):
-        return Crawl.objects.create(title="testcrawl1")
-
     def test_crawl(self):
-        u = self.setUp()
+        data = {"username": "test_u", "email": "1@gmail.com", "password": "test_pw"}
+        user = User.objects.create(**data)
+        u = Crawl.objects.create(id=1, title="testcrawl1", author=user)
         self.assertEquals("testcrawl1", u.title)
         self.assertEquals(str(u), u.title)
 
@@ -28,8 +28,14 @@ class TestModels(TestCase):
         self.assertEquals(str(u), u.title)
 
     def setUpCP(self):
+        data = {
+            "username": "test_u2",
+            "email": "test2@gmail.com",
+            "password": "test_pw",
+        }
+        user = User.objects.create(**data)
         return CrawlPoint.objects.create(
-            crawl=Crawl.objects.create(title="testcrawl1"),
+            crawl=Crawl.objects.create(id=2, title="testcrawl1", author=user),
             point=Point.objects.create(title="testpoint1"),
         )
 
@@ -39,9 +45,15 @@ class TestModels(TestCase):
         self.assertEquals("testpoint1", u.point.title)
 
     def setUpCT(self):
+        data = {
+            "username": "test_u3",
+            "email": "test3@gmail.com",
+            "password": "test_pw",
+        }
+        user = User.objects.create(**data)
         return CrawlTag.objects.create(
             tag=Tag.objects.create(title="testtag1"),
-            point=Crawl.objects.create(title="testcrawl1"),
+            point=Crawl.objects.create(id=3, title="testcrawl1", author=user),
         )
 
     def test_CT(self):
