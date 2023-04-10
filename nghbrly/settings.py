2320ce5d05d89d3d436b846ec6044934260c17f5
ismaeltocruz <ismaeltocruz@gmail.com>
2023-04-10 09:40:02 -0400

    formatting

--- nghbrly/settings.py
+++ nghbrly/settings.py
@@ -181,8 +181,8 @@ EMAIL_USE_SSL = True
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = 'nghbrly-2'
+AWS_STORAGE_BUCKET_NAME = "nghbrly-2"
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
