790dd89e09b2007c946b3941a75e305e8f787960
Hani <hani.m17@iiits.in>
2023-04-10 10:06:18 -0400

    fix username change errors

--- crawls/models.py
+++ crawls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from api.models import User
 
 # Create your models here.
 
@@ -7,7 +8,7 @@ class Crawl(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=500, null=True)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField("Tag", through="CrawlTag", blank=True)
